253373875b900ee04ea829f8b2963cae74417dc5
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2022-12-18 10:36:02 +1100

    Remove File

--- kubernetes_dashboard/install_dashboard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm repo update
-helm install kube-dashboard kubernetes-dashboard/kubernetes-dashboard -f values.yaml
